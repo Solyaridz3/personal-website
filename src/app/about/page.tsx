5b52bfbb906ffb870f987b4090d53a3b50389d95
solyaridz3 <solyaridz3@gmail.com>
2024-09-23 11:10:38 +0300

    udpated resume

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,46 +2,46 @@ import Image from "next/image";
 
 function Page() {
     return (
-        <main
-            className="bg-gradient-radial from-gray-900 to-black border-t-2 border-gray-800 flex min-h-screen md:flex-row flex-col items-center justify-center pt-20 gap-10 md:p-8 md:gap-32">
-            <div
-                className="shadow-[0px_0px_60px_rgba(100,0,255,0.3)] rounded-md">
-                <Image src="/me.jpg" alt="me" height={200} width={200}
-                       className={"rounded-md object-cover h-full w-full md:drop-shadow-[10px_0px_500px_rgba(100,0,255,0.8)]"}/>
+        <main className="bg-gradient-radial from-gray-900 to-black border-t-2 border-gray-800 flex min-h-screen md:flex-row flex-col items-center justify-center pt-20 gap-10 md:p-8 md:gap-32">
+            <div className="shadow-[0px_0px_60px_rgba(100,0,255,0.3)] rounded-md">
+                <Image
+                    src="/me.jpg"
+                    alt="me"
+                    height={200}
+                    width={200}
+                    className={
+                        "rounded-md object-cover h-full w-full md:drop-shadow-[10px_0px_500px_rgba(100,0,255,0.8)]"
+                    }
+                />
             </div>
             <div className="text-xl md:w-2/3 p-10">
                 <p>
-                    My name is Serhii, and I am a 19-year-old student from Kyiv with a deep passion for
-                    programming and a fervent desire to contribute meaningfully to the field. I would like to
-                    share with you my background, skills, and motivation.
-
-                    Currently pursuing a bachelor&apos;s degree in Cyber security at National Aviation University, I
-                    have dedicated a significant portion of my academic journey to acquiring knowledge and
-                    skills that span programming domains. In addition to my formal education, I have actively
-                    engaged in self-directed learning, particularly in the realm of node.js development using
-                    JavaScript and TypeScript. My primary focus lies in honing my abilities to create robust
-                    APIs and develop user-friendly frontends with HTML, CSS, and React.
-
-                    One of the facets of my educational journey includes current participation in peer-to-peer
-                    courses offered by the volunteer organization SHPP. These courses, centered around computer
-                    science and node.js development.
-
-                    I am excited about the prospect of applying my skills to real-world challenges and
-                    contributing to innovative projects.
-
-                    I am confident that my blend of academic knowledge, technical skills, and proactive approach
-                    makes me well-suited for the challenges that Node.js Development Course presents. I am eager
-                    to bring my enthusiasm for programming, my dedication to continuous learning, and my
-                    problem-solving mindset to your esteemed team.
-
-                    I would welcome the opportunity to further discuss how my skills and experiences align with
-                    the needs of Solvd. Thank you for considering my application. I am looking forward to the
-                    possibility of contributing to the success of your team.
+                    A dedicated and detail-oriented
+                    Junior Backend Developer with a robust foundation in
+                    Node.js, JavaScript, TypeScript, Express, PostgreSQL,
+                    MongoDB, Docker, and Git. I possess practical experience in
+                    designing and developing RESTful APIs, along with a solid
+                    understanding of core data structures and algorithms
+                    essential for backend development. I began exploring
+                    programming at the age of 13, and while it sparked my
+                    interest early on, I have focused intensively on developing
+                    my skills over the past year. During this time, I have
+                    gained hands-on experience with unit and end-to-end (E2E)
+                    testing, as well as basic exposure to AWS services like EC2
+                    and S3. My commitment to continuous learning is demonstrated
+                    by my participation in a selective training program at
+                    SOLVD, where I have been refining my expertise over the last
+                    five months. In addition to my technical abilities, I have a
+                    B2 level of English proficiency and excellent communication
+                    skills, allowing me to effectively articulate complex ideas
+                    and collaborate within a team. I am now seeking a junior
+                    developer role where I can leverage my skills, contribute to
+                    impactful projects, and grow professionally within a
+                    collaborative and innovative environment.
                 </p>
             </div>
-
         </main>
-    )
+    );
 }
 
 export default Page;
